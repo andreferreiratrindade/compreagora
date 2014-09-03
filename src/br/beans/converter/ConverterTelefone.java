3ba981718cbb417d2081172248aaecb75aaefb70
André <andreferreiratrindade@gmail.com>
2014-09-03 10:11:08 -0300

    implementando bootStrap nas páginas de Admin Empresa

--- src/br/beans/converter/ConverterTelefone.java
+++ src/br/beans/converter/ConverterTelefone.java
@@ -1,4 +1,5 @@
 package br.beans.converter;
+
 import java.io.Serializable;
 
 import javax.faces.component.UIComponent;
@@ -6,25 +7,28 @@ import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
-@FacesConverter(value="converterTelefone")  
-public class ConverterTelefone implements Converter,Serializable{  
-  
-    /**
+@FacesConverter(value = "converterTelefone")
+public class ConverterTelefone implements Converter, Serializable {
+
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	@Override  
-    public Object getAsObject(FacesContext arg0, UIComponent arg1, String valor) {  
-        if(valor != null || valor != "") { 
-        	valor = valor.toString().replaceAll("[- ()()]", "");
-            valor = valor.toString().replaceAll("[- ()]", "");            
-        }  
-        return valor;  
-    }  
-  
-    @Override  
-    public String getAsString(FacesContext arg0, UIComponent arg1, Object valor) {  
-        return InputMask.stringToMask(InputMask.TELEFONE, valor.toString());  
-    }  
-}  
+	@Override
+	public Object getAsObject(FacesContext arg0, UIComponent arg1, String valor) {
+		if (valor != null || valor != "") {
+			valor = valor.toString().replaceAll("[- ()()]", "");
+			valor = valor.toString().replaceAll("[- ()]", "");
+		}
+		return valor;
+	}
+
+	@Override
+	public String getAsString(FacesContext arg0, UIComponent arg1, Object valor) {
+		if (valor != null || !valor.toString().isEmpty()) {
+			return InputMask.stringToMask(InputMask.TELEFONE, valor.toString());
+		}
+		return  valor.toString();
+	}
+}
